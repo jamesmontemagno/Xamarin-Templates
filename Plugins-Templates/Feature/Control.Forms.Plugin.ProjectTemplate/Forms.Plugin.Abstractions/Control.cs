5e878b9e96eb82ddbf8b379efed2f39874aaea4e
James Montemagno <james.montemagno@gmail.com>
2014-12-19 11:18:43 -0800

    Update template with lower version number

---
+++ Plugins-Templates/Feature/Control.Forms.Plugin.ProjectTemplate/Forms.Plugin.Abstractions/Control.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace $safeprojectname$
+{
+    /// <summary>
+    /// $safeprojectgroupname$ Interface
+    /// </summary>
+    public class $safeprojectgroupname$Control //: Control type
+    {
+      //example of custom property
+      /// <summary>
+      /// Thickness property of border
+      /// </summary>
+      //public static readonly BindableProperty BorderThicknessProperty =
+      //  BindableProperty.Create<$safeprojectgroupname$Image, int>(
+      //    p => p.BorderThickness, 0);
+
+      /// <summary>
+      /// Border thickness of circle image
+      /// </summary>
+      //public int BorderThickness
+      //{
+      //  get { return (int)GetValue(BorderThicknessProperty); }
+      //  set { SetValue(BorderThicknessProperty, value); }
+      //}
+  }
+}
---
+++ Plugins-Templates/Feature/Control.Forms.Plugin.ProjectTemplate/Forms.Plugin.WindowsPhone/FeatureImplementation.cs
@@ -0,0 +1,20 @@
+﻿using $safeprojectgroupname$.Forms.Plugin.Abstractions;
+using System;
+using Xamarin.Forms;
+using $safeprojectname$;
+using Xamarin.Forms.Platform.WinPhone;
+
+[assembly: ExportRenderer(typeof($safeprojectgroupname$.Forms.Plugin.Abstractions.$safeprojectgroupname$Control), typeof($safeprojectgroupname$Renderer))]
+namespace $safeprojectname$
+{
+  /// <summary>
+  /// $safeprojectgroupname$ Renderer
+  /// </summary>
+  public class $safeprojectgroupname$Renderer //: TRender (replace with renderer type
+  {
+    /// <summary>
+    /// Used for registration with dependency service
+    /// </summary>
+    public static void Init(){}
+  }
+}
