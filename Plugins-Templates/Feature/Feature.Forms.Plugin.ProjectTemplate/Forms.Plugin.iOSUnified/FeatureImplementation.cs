a39d6acb40119bfbe664455a50ed50b8ddedebc0
James Montemagno <james.montemagno@gmail.com>
2015-01-13 16:29:21 -0500

    Add unified and 1.3.1 to forms

---
+++ Plugins-Templates/Feature/Feature.Forms.Plugin.ProjectTemplate/Forms.Plugin.iOSUnified/FeatureImplementation.cs
@@ -0,0 +1,19 @@
+﻿using $safeprojectgroupname$.Forms.Plugin.Abstractions;
+using System;
+using Xamarin.Forms;
+using $safeprojectname$;
+
+[assembly: Dependency(typeof($safeprojectgroupname$Implementation))]
+namespace $safeprojectname$
+{
+  /// <summary>
+  /// $safeprojectgroupname$ Implementation
+  /// </summary>
+  public class $safeprojectgroupname$Implementation : I$safeprojectgroupname$
+  {
+    /// <summary>
+    /// Used for registration with dependency service
+    /// </summary>
+    public static void Init(){}
+  }
+}
