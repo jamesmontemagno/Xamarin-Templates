e4660505f75f49ce4971292ec0fcdabf743151b0
Martijn van Dijk <mhvdijk@gmail.com>
2016-01-19 13:56:05 +0100

    Add UWP to Forms plugin

---
+++ Plugins-Templates/Feature/Feature.Forms.Plugin.Uwp/FeatureImplementation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.Forms;
+using Feature.Forms.Plugin.Abstractions;
+using Feature.Forms.Plugin.Uwp;
+
+[assembly: Dependency(typeof(FeatureImplementation))]
+namespace Feature.Forms.Plugin.Uwp
+{
+  public class FeatureImplementation : IFeature
+  {
+
+  }
+}
