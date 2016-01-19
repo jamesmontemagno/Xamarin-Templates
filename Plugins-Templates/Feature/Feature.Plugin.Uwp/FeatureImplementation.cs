1b5d3aa6a51a512a16931e54899aee0f182720eb
Martijn van Dijk <mhvdijk@gmail.com>
2016-01-19 12:55:06 +0100

    Add UWP project

---
+++ Plugins-Templates/Feature/Feature.Plugin.Uwp/FeatureImplementation.cs
@@ -0,0 +1,13 @@
+using Feature.Plugin.Abstractions;
+using System;
+
+
+namespace Feature.Plugin
+{
+  /// <summary>
+  /// Implementation for Feature
+  /// </summary>
+  public class FeatureImplementation : IFeature
+  {
+  }
+}
