85079a064b9ff5e6892203e0e2585f19378f1873
Martijn van Dijk <mhvdijk@gmail.com>
2016-01-27 16:11:05 +0100

    Fix UWP projects

---
+++ Plugins-Templates/Feature/Feature.Plugin.ProjectTemplate/Feature.Plugin.Uwp/FeatureImplementation.cs
@@ -0,0 +1,13 @@
+using Plugin.$safeprojectgroupname$.Abstractions;
+using System;
+
+
+namespace Plugin.$safeprojectgroupname$
+{
+  /// <summary>
+  /// Implementation for Feature
+  /// </summary>
+  public class $safeprojectgroupname$Implementation : I$safeprojectname$
+  {
+  }
+}
