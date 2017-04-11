7f4e907d92f5da4af9915644aa89f06293857a91
James Montemagno <james.montemagno@gmail.com>
2017-04-11 11:48:03 -0700

    Mass renaming

--- Templates/Properties/AssemblyInfo.cs
+++ Templates/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Feature.Plugin.ProjectTemplate")]
+[assembly: AssemblyTitle("Plugin.ProjectTemplate")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Feature.Plugin.ProjectTemplate")]
+[assembly: AssemblyProduct("Plugin.ProjectTemplate")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
