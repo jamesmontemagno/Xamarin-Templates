8ad210587bf7556de159eb52d54e597d79046cad
James Montemagno <james.montemagno@gmail.com>
2017-08-02 17:59:02 -0700

    Add true blank app and single view app

--- Xamarin.Android-Templates/Projects/BlankAppCompat/BlankAppCompat/MainActivity.cs
+++ Xamarin.Android-Templates/Projects/BlankAppCompat/BlankAppCompat/MainActivity.cs
@@ -5,45 +5,20 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Support.V7.App;
 
 namespace BlankAppCompat
 {
     [Activity(Label = "BlankAppCompat", MainLauncher = true, LaunchMode = Android.Content.PM.LaunchMode.SingleTop, Icon = "@drawable/icon")]
-    public class MainActivity : BaseActivity
+    public class MainActivity : AppCompatActivity
     {
-
-        protected override int LayoutResource
-        {
-            get { return Resource.Layout.main; }
-        }
-        int count = 1;
-
+        
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
-            // Get our button from the layout resource,
-            // and attach an event to it
-            var clickButton = FindViewById<Button>(Resource.Id.my_button);
-
-            clickButton.Click += (sender, args) =>
-              {
-                  clickButton.Text = string.Format("{0} clicks!", count++);
-              };
-
-            var navigationButton = FindViewById<Button>(Resource.Id.nav_button);
-
-            navigationButton.Click += (sender, args) =>
-              {
-                  var intent = new Intent(this, typeof(SecondActivity));
-                  intent.PutExtra("clicks", count);
-                  StartActivity(intent);
-              };
-
-
-
-            SupportActionBar.SetDisplayHomeAsUpEnabled(false);
-            SupportActionBar.SetHomeButtonEnabled(false);
+            //Set your main view here
+            //SetContentView(Resource.Layout.main);
 
         }
     }
--- Xamarin.Android-Templates/Projects/SingleViewApp/BlankAppCompat/Resources/Resource.Designer.cs
+++ Xamarin.Android-Templates/Projects/SingleViewApp/BlankAppCompat/Resources/Resource.Designer.cs
@@ -1712,8 +1712,8 @@ namespace BlankAppCompat
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f0a0059
-			public const int action0 = 2131361881;
+			// aapt resource value: 0x7f0a005c
+			public const int action0 = 2131361884;
 			
 			// aapt resource value: 0x7f0a0047
 			public const int action_bar = 2131361863;
@@ -1736,17 +1736,17 @@ namespace BlankAppCompat
 			// aapt resource value: 0x7f0a0024
 			public const int action_bar_title = 2131361828;
 			
-			// aapt resource value: 0x7f0a0056
-			public const int action_container = 2131361878;
+			// aapt resource value: 0x7f0a0059
+			public const int action_container = 2131361881;
 			
 			// aapt resource value: 0x7f0a0048
 			public const int action_context_bar = 2131361864;
 			
-			// aapt resource value: 0x7f0a005d
-			public const int action_divider = 2131361885;
+			// aapt resource value: 0x7f0a0060
+			public const int action_divider = 2131361888;
 			
-			// aapt resource value: 0x7f0a0057
-			public const int action_image = 2131361879;
+			// aapt resource value: 0x7f0a005a
+			public const int action_image = 2131361882;
 			
 			// aapt resource value: 0x7f0a0002
 			public const int action_menu_divider = 2131361794;
@@ -1763,11 +1763,11 @@ namespace BlankAppCompat
 			// aapt resource value: 0x7f0a0026
 			public const int action_mode_close_button = 2131361830;
 			
-			// aapt resource value: 0x7f0a0058
-			public const int action_text = 2131361880;
+			// aapt resource value: 0x7f0a005b
+			public const int action_text = 2131361883;
 			
-			// aapt resource value: 0x7f0a0066
-			public const int actions = 2131361894;
+			// aapt resource value: 0x7f0a0069
+			public const int actions = 2131361897;
 			
 			// aapt resource value: 0x7f0a0027
 			public const int activity_chooser_view_content = 2131361831;
@@ -1790,14 +1790,14 @@ namespace BlankAppCompat
 			// aapt resource value: 0x7f0a002e
 			public const int buttonPanel = 2131361838;
 			
-			// aapt resource value: 0x7f0a005a
-			public const int cancel_action = 2131361882;
+			// aapt resource value: 0x7f0a005d
+			public const int cancel_action = 2131361885;
 			
 			// aapt resource value: 0x7f0a003e
 			public const int checkbox = 2131361854;
 			
-			// aapt resource value: 0x7f0a0062
-			public const int chronometer = 2131361890;
+			// aapt resource value: 0x7f0a0065
+			public const int chronometer = 2131361893;
 			
 			// aapt resource value: 0x7f0a001e
 			public const int collapseActionView = 2131361822;
@@ -1826,8 +1826,8 @@ namespace BlankAppCompat
 			// aapt resource value: 0x7f0a001b
 			public const int end = 2131361819;
 			
-			// aapt resource value: 0x7f0a006c
-			public const int end_padder = 2131361900;
+			// aapt resource value: 0x7f0a006f
+			public const int end_padder = 2131361903;
 			
 			// aapt resource value: 0x7f0a0028
 			public const int expand_activities_button = 2131361832;
@@ -1844,8 +1844,8 @@ namespace BlankAppCompat
 			// aapt resource value: 0x7f0a002c
 			public const int icon = 2131361836;
 			
-			// aapt resource value: 0x7f0a0067
-			public const int icon_group = 2131361895;
+			// aapt resource value: 0x7f0a006a
+			public const int icon_group = 2131361898;
 			
 			// aapt resource value: 0x7f0a001f
 			public const int ifRoom = 2131361823;
@@ -1853,14 +1853,14 @@ namespace BlankAppCompat
 			// aapt resource value: 0x7f0a0029
 			public const int image = 2131361833;
 			
-			// aapt resource value: 0x7f0a0063
-			public const int info = 2131361891;
+			// aapt resource value: 0x7f0a0066
+			public const int info = 2131361894;
 			
-			// aapt resource value: 0x7f0a0068
-			public const int line1 = 2131361896;
+			// aapt resource value: 0x7f0a006b
+			public const int line1 = 2131361899;
 			
-			// aapt resource value: 0x7f0a006a
-			public const int line3 = 2131361898;
+			// aapt resource value: 0x7f0a006d
+			public const int line3 = 2131361901;
 			
 			// aapt resource value: 0x7f0a0009
 			public const int listMode = 2131361801;
@@ -1868,14 +1868,20 @@ namespace BlankAppCompat
 			// aapt resource value: 0x7f0a002b
 			public const int list_item = 2131361835;
 			
-			// aapt resource value: 0x7f0a005c
-			public const int media_actions = 2131361884;
+			// aapt resource value: 0x7f0a0057
+			public const int main_content = 2131361879;
+			
+			// aapt resource value: 0x7f0a005f
+			public const int media_actions = 2131361887;
 			
 			// aapt resource value: 0x7f0a001c
 			public const int middle = 2131361820;
 			
 			// aapt resource value: 0x7f0a0014
 			public const int multiply = 2131361812;
+			
+			// aapt resource value: 0x7f0a0058
+			public const int my_button = 2131361880;
 			
 			// aapt resource value: 0x7f0a0020
 			public const int never = 2131361824;
@@ -1886,14 +1892,14 @@ namespace BlankAppCompat
 			// aapt resource value: 0x7f0a000a
 			public const int normal = 2131361802;
 			
-			// aapt resource value: 0x7f0a0065
-			public const int notification_background = 2131361893;
+			// aapt resource value: 0x7f0a0068
+			public const int notification_background = 2131361896;
 			
-			// aapt resource value: 0x7f0a005f
-			public const int notification_main_column = 2131361887;
+			// aapt resource value: 0x7f0a0062
+			public const int notification_main_column = 2131361890;
 			
-			// aapt resource value: 0x7f0a005e
-			public const int notification_main_column_container = 2131361886;
+			// aapt resource value: 0x7f0a0061
+			public const int notification_main_column_container = 2131361889;
 			
 			// aapt resource value: 0x7f0a0030
 			public const int parentPanel = 2131361840;
@@ -1907,11 +1913,11 @@ namespace BlankAppCompat
 			// aapt resource value: 0x7f0a0040
 			public const int radio = 2131361856;
 			
-			// aapt resource value: 0x7f0a0064
-			public const int right_icon = 2131361892;
+			// aapt resource value: 0x7f0a0067
+			public const int right_icon = 2131361895;
 			
-			// aapt resource value: 0x7f0a0060
-			public const int right_side = 2131361888;
+			// aapt resource value: 0x7f0a0063
+			public const int right_side = 2131361891;
 			
 			// aapt resource value: 0x7f0a0015
 			public const int screen = 2131361813;
@@ -1985,8 +1991,8 @@ namespace BlankAppCompat
 			// aapt resource value: 0x7f0a0018
 			public const int src_over = 2131361816;
 			
-			// aapt resource value: 0x7f0a005b
-			public const int status_bar_latest_event_content = 2131361883;
+			// aapt resource value: 0x7f0a005e
+			public const int status_bar_latest_event_content = 2131361886;
 			
 			// aapt resource value: 0x7f0a0041
 			public const int submenuarrow = 2131361857;
@@ -1997,11 +2003,11 @@ namespace BlankAppCompat
 			// aapt resource value: 0x7f0a000b
 			public const int tabMode = 2131361803;
 			
-			// aapt resource value: 0x7f0a006b
-			public const int text = 2131361899;
+			// aapt resource value: 0x7f0a006e
+			public const int text = 2131361902;
 			
-			// aapt resource value: 0x7f0a0069
-			public const int text2 = 2131361897;
+			// aapt resource value: 0x7f0a006c
+			public const int text2 = 2131361900;
 			
 			// aapt resource value: 0x7f0a0035
 			public const int textSpacerNoButtons = 2131361845;
@@ -2009,8 +2015,8 @@ namespace BlankAppCompat
 			// aapt resource value: 0x7f0a0034
 			public const int textSpacerNoTitle = 2131361844;
 			
-			// aapt resource value: 0x7f0a0061
-			public const int time = 2131361889;
+			// aapt resource value: 0x7f0a0064
+			public const int time = 2131361892;
 			
 			// aapt resource value: 0x7f0a002d
 			public const int title = 2131361837;
@@ -2020,6 +2026,9 @@ namespace BlankAppCompat
 			
 			// aapt resource value: 0x7f0a003a
 			public const int title_template = 2131361850;
+			
+			// aapt resource value: 0x7f0a0056
+			public const int toolbar = 2131361878;
 			
 			// aapt resource value: 0x7f0a0023
 			public const int top = 2131361827;
@@ -2220,6 +2229,9 @@ namespace BlankAppCompat
 			
 			// aapt resource value: 0x7f03002e
 			public const int support_simple_spinner_dropdown_item = 2130903086;
+			
+			// aapt resource value: 0x7f03002f
+			public const int toolbar = 2130903087;
 			
 			static Layout()
 			{
